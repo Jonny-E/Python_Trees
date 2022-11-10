076bc5c96840171ba8b0fe26e581924a66892c94
Jonny-E <jonahsimon06@gmail.com>
2022-11-10 10:05:28 +0100

    Files_Renamed

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,67 +1,122 @@
-class BinarySearchTree:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
+class binarytree( ):
+    def __init__(self,pContent = None ,pLefttree = None,pRighttree = None):
+        self.content = pContent
+        self.lefttree = pLefttree
+        self.righttree = pRighttree
 
-
-    def insert(self, data):
-        if data < self.data:
-            if self.left:
-                self.left.insert(data)
-            else:
-                self.left = BinarySearchTree(data)
-                return
+    def empty(self):
+        if self.content :
+            return False 
         else:
-            if self.right:
-                self.right.insert(data)
-            else:
-                self.right = BinarySearchTree(data)
-                return
+            return True
 
 
-    def search(self, val):
-        if val==self.data:
-            return str(val)+" is found"
-        elif val < self.data:
-            if self.left:
-                return self.left.search(val)
-            else:
-                return str(val)+" is not found"
+    def setcontent(self,tempcon):   
+        if self.epmty():
+            self.content = tempcon
 
         else:
-            if self.right:
-                return self.right.search(val)
+            self.content = tempcon
+            self.lefttree = binarytree()
+            self.righttree = binarytree()
+
+    def getcon(self):
+        print(self.content)
+
+    def setlefttree(self,temptree):
+        if temptree != None:
+            if self.empty():
+                return None
             else:
-                return str(val)+" is not found" 
-       
+                self.lefttree = temptree
+        else:
+            return None
+
+    def setrighttree(self,temptree):
+        if temptree != None:
+            if self.empty():
+              return None
+            else:
+                self.righttree = temptree
+        else:
+            return None
+
+
+    def getlefttree(self):
+        if self.lefttree is None:
+            return None
+        else:
+            return self.lefttree
+
+    def getrighttree(self):
+        if self.righttree is None:
+            return None
+        else:
+            return self.righttree
 
     def preorder(self):
-        if self.left:
-            self.left.preorder()
-        print( self.data)
-        if self.right:
-            self.right.preorder()
-
-    def inorder(self):
-        pass
-
-
-
+        self.getcon()
+        if self.lefttree != None :
+            self.lefttree.preorder()
+        
+        if self.righttree != None:
+            self.righttree.preorder()
     
 
+    def inorder(self):
+        
+        if self.lefttree != None:
+            self.lefttree.inorder()
+
+        self.getcon()
+        
+
+        if self.righttree != None:
+            self.righttree.inorder()
+    
+        
+    def postorder(self):
+        if self.lefttree != None:
+            self.lefttree.postorder()
+
+        if self.righttree != None:
+            self.righttree.postorder()
+
+        self.getcon()
+        
+    def getpreorder(self) :
+        print("Preorder:      ")
+        self.preorder()
+        
+
+    def getinorder(self) :
+        print("Inorder:      ")
+        self.inorder()
+    
+
+    def getpostorder(self) :
+        print("Postorder:      ")
+        self.postorder()
+      
+    def getallorders(self):   
+        self.getpreorder() 
+        print("\n")
+        self.getinorder()
+        print("\n")
+        self.getpostorder()
+
+    def remove(self, searchcon):
+        if self.content == searchcon:
+            print("true")
+        
 
 
-root = BinarySearchTree(27)
-
-root.insert(5)
-root.insert(3)
-root.insert(7)
-root.insert(10)
-root.insert(19)
-root.preorder()
-print("\n")
-root.inorder()
-
-
-
+  
+baum_1= binarytree(5)
+baum_1.setlefttree(binarytree(3))
+baum_1.lefttree.setlefttree(binarytree(2))
+baum_1.lefttree.setrighttree(binarytree(1))
+baum_1.setrighttree(binarytree(7))
+baum_1.righttree.setlefttree(binarytree(4))
+            
+baum_1.remove(5)
